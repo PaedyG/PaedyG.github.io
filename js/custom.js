911fea210bcd9689a94259d2f602a28d9f44aad6
Gemperle Patrick ABRAXAS <patrick.gemperle@abraxas.ch>
2019-09-11 10:24:21 +0200

    moved some js to custom.js

---
+++ js/custom.js
@@ -0,0 +1,41 @@
+//Script for Header-Text fade-in
+$(document).ready(function () {
+    $("#myname").fadeIn(3000); 
+    $("#thatsme").fadeIn(5000);
+});
+
+
+//Script for the radial-menu animations
+$(document).ready(function() {
+    var is_menu_open = false;
+
+    $(".radial_menu").on("click", function() {
+
+        if (!is_menu_open) {
+            $(".menu_item").css({
+                'background-color': 'gray', 
+                'transform': 'translate(60px,105px)'
+            });
+
+            $(".fa.fa-empire").fadeIn(1000);
+
+            $(".fa.fa-home").css({
+                'display': 'none'
+            });
+
+        } else {
+            $(".menu_item").css({
+                'background-color': 'darkGray', 
+                'transform': 'none'
+            });
+
+            $(".fa.fa-home").fadeIn(1000);
+
+            $(".fa.fa-empire").css({
+                'display': 'none'
+            });
+        }
+        
+        is_menu_open = !is_menu_open;
+    });
+});
