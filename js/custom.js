d84330f87a3c7a63061dcc9699360f6df59b93e3
Gemperle Patrick ABRAXAS <patrick.gemperle@abraxas.ch>
2019-09-12 18:15:32 +0200

    testing something...

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 /*-----Script for Header-Text fade-in-----*/
 $(document).ready(function () {
     $("#myname").fadeIn(3000); 
-    $("#thatsme").fadeIn(5000);
+    $("#thatsme").fadeIn(4000);
 });
 
 /*-----Script for the radial-menu animations-----*/
